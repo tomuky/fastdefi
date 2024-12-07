1e9c341ed014094743829b0a5f7f7d4e72d2a00b
Tom <mrtomd@gmail.com>
2024-12-07 00:29:25 -0500

    basename in account bar

--- app/(pages)/(topics)/get-a-username/page.js
+++ app/(pages)/(topics)/get-a-username/page.js
@@ -2,9 +2,11 @@
 import classes from '@/app/(pages)/Pages.module.css';
 import { useAccount } from 'wagmi';
 import { useEffect, useState } from 'react';
-import { getBasename, getBasenameAvatar, getBasenameTextRecord, BasenameTextRecordKeys } from '@/app/_apis/Basenames';
+import { getBasename, getBasenameAvatar, getBasenameTextRecord, BasenameTextRecordKeys } from '@/app/_apis/basenames';
 import ListNote from '@/app/components/ListNote';
 import ListFinish from '@/app/components/ListFinish';
+import NextButton from '@/app/components/NextButton';
+import Spacer from '@/app/components/Spacer';
 
 export default function GetAUsername() {
     const { address, isConnected } = useAccount();
@@ -40,9 +42,14 @@ export default function GetAUsername() {
                     <li>Make sure "Set as primary" is checked</li>
                     <ListNote>This allows your username to be used as your address</ListNote>
                     <li>Click Register Name and confirm the popup in your wallet</li>
+                    {basename && <li>Complete more of your profile on <a href={`https://www.base.org/name/${basename}`} target="_blank" rel="noopener noreferrer">base.org/name/{basename}</a></li>}
                     {basename && <ListFinish>Done! Your new username on Base is {basename}</ListFinish>}
                 </ol>
             </div>
+
+            <NextButton title="Use Basescan" target='/use-basescan'/>
+
+            <Spacer/>
 
         </div>
     );
--- app/components/AccountAddress.js
+++ app/components/AccountAddress.js
@@ -1,25 +1,35 @@
 import classes from './Account.module.css';
 import { useAccountModal } from '@rainbow-me/rainbowkit';
-import { useAccount,useEnsName } from 'wagmi';
+import { useAccount } from 'wagmi';
 import { useRouter } from 'next/navigation';
+import { getBasename } from '@/app/_apis/basenames';
+import { useEffect, useState } from 'react';
+
 const AccountAddress = () => {
     const router = useRouter();
-    const { address } = useAccount();
+    const { address, isConnected } = useAccount();
     const { openAccountModal } = useAccountModal();
-    const { data: ensName, isLoading } = useEnsName({
-        address: address,
-        chainId: 1, // Always query mainnet for ENS
-    });
+    const [basename, setBasename] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            if (!isConnected) return;
+            const fetchedBasename = await getBasename(address);
+            setBasename(fetchedBasename);
+        }
+        fetchData();
+    }, [address, isConnected]);
 
     return (
         <div className={classes.addressArea} onClick={openAccountModal}>
             <div className={classes.addressLabel}>
                 <img src="/images/ui/user.png" alt="User" className={classes.accountImage} />
-                Account
+                {basename && basename.length <= 18 && 'Account'}
             </div>
-            <span className={classes.address} style={{ fontSize: ensName && ensName.length >= 17 ? '0.8em' : '1em' }}>
-                {/* {ensName && ensName.length >= 17 ? `${ensName.slice(0, 7)}...${ensName.slice(-7)}` : ensName || `${address.slice(0, 5)}...${address.slice(-4)}`} */}
-                {`${address.slice(0, 5)}...${address.slice(-4)}`}
+            <span className={classes.address} 
+                style={{ fontSize: basename && basename.length >= 13 ? '0.9em' : '1em' }}>
+                {!basename && `${address.slice(0, 5)}...${address.slice(-4)}`}
+                {basename && basename}
             </span>
         </div>
     )
