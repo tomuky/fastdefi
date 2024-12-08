88ccd2ad998b6941c19c7ad754f5c4249da8eae5
Tom <mrtomd@gmail.com>
2024-12-08 14:10:29 -0500

    updating packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        "encoding": false,
+        "crypto": false,
+        "stream": false,
+        "http": false,
+        "https": false,
+        "os": false,
+        "url": false
+      }
+      return config
+    },
+};
 
 export default nextConfig;
