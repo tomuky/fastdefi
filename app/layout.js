f98c882d72c79d62c3b3ddadde313dfdff5dc94a
Tom <mrtomd@gmail.com>
2024-12-11 18:20:50 -0500

    add social image

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,32 @@ import "./globals.css";
 import { Providers } from './providers'
 import { Analytics } from '@vercel/analytics/react'
 
+export const metadata = {
+  title: 'Fast DeFi',
+  description: 'Learn DeFi by trying',
+  openGraph: {
+    title: 'Fast DeFi',
+    description: 'Learn DeFi by trying',
+    url: 'https://www.fast-defi.com',
+    siteName: 'Fast DeFi',
+    images: [
+      {
+        url: 'https://www.fast-defi.com/images/fdt-social-1200-x630.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Fast DeFi',
+    description: 'Learn DeFi by trying',
+    images: ['https://www.fast-defi.com/images/fdt-social-1200-x630.png'], // Replace with your image path
+  },
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
