96e6d70d5ab6e0fca51b2e0bd224a22334254f2f
Tom <mrtomd@gmail.com>
2025-12-20 07:22:51 -0500

    loading animation in footer mobile

--- app/components/FooterMobile.js
+++ app/components/FooterMobile.js
@@ -1,13 +1,23 @@
 'use client'
+import { useState } from 'react';
 import classes from './FooterMobile.module.css';
 import Image from 'next/image';
 
 const FooterMobile = ({message, onClick, isNextTopic}) => {
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleClick = () => {
+        if (isNextTopic && !isLoading) {
+            setIsLoading(true);
+        }
+        onClick?.();
+    };
+
     return (
-        <div className={classes.footer}>
+        <div className={`${classes.footer} ${isLoading ? classes.loading : ''}`}>
             <div 
                 className={`${classes.button} ${isNextTopic ? classes.nextTopic : ''}`} 
-                onClick={onClick}
+                onClick={handleClick}
             >
                 <span>{message}</span>
                 <Image src="/images/ui/right-arrow.png" alt="right arrow" className={classes.icon} width={20} height={20} />
